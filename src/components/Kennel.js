6111e2ebab0a434de715566e1e0e28c101b428d8
heavenburdette1988 <heavenburdette1988@gmail.com>
2021-12-06 15:45:59 -0500

    finsihed ch 10

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,67 +1,104 @@
-import React from "react"
-// import { AnimalCard } from "./animal/AnimalCard"
-import "./animal/Animal.css"
-// import {CustomerCard} from "./customer/Customer"
-// import { LocationCard } from "./location/Location"
-// import { EmployeeCard } from "./employee/Employee"
-import { PropsAndState } from "./PropsAndState"
-import { NavBar } from "./nav/NavBar"
-import { ApplicationViews } from "./ApplicationViews"
+import React, { useState } from "react";
+import { NavBar } from "./nav/NavBar";
+import { ApplicationViews } from "./ApplicationViews";
+import "./Kennel.css";
+import { Routes, Route, Navigate } from "react-router";
+import { Login } from "./auth/Login";
+import { Register } from "./auth/Register";
 
-export const Kennel = () => (
-    
-    <>
-     <>
+export const Kennel = () => {
+  const [loggedin, setLoggedin] = useState(false);
+
+  const changeState = (bool) => setLoggedin(bool);
+
+  if (localStorage.getItem("kennel_customer")) {
+    return (
+      <>
         <NavBar />
         <ApplicationViews />
-    </>
+      </>
+    );
+  } else {
+    return (
+      <Routes>
+        <Route path="/" element={<Navigate to="login" />} />
+        <Route path="/login" element={<Login setLoggedin={changeState} />} />
+        <Route path="/register" element={<Register setLoggedin={changeState} />} />
+      </Routes>
+    );
+  }
+};
+
+
+
+
+
+
+
+// import React from "react"
+// // import { AnimalCard } from "./animal/AnimalCard"
+// import "./animal/Animal.css"
+// // import {CustomerCard} from "./customer/Customer"
+// // import { LocationCard } from "./location/Location"
+// // import { EmployeeCard } from "./employee/Employee"
+// import { PropsAndState } from "./PropsAndState"
+// import { NavBar } from "./nav/NavBar"
+// import { ApplicationViews } from "./ApplicationViews"
+
+// export const Kennel = () => (
+    
+//     <>
+//      <>
+//         <NavBar />
+//         <ApplicationViews />
+//     </>
        
 
-        {/* <h2>Nashville Kennels</h2>
-<small>Loving care when you're not there.</small>
+//         {/* <h2>Nashville Kennels</h2>
+// <small>Loving care when you're not there.</small>
 
-<address>
-    <div>Visit Us at the Nashville North Location</div>
-    <div>500 Puppy Way</div>
-</address> */}
-        {/* <PropsAndState yourName={"Heaven"} /> */}
+// <address>
+//     <div>Visit Us at the Nashville North Location</div>
+//     <div>500 Puppy Way</div>
+// </address> */}
+//         {/* <PropsAndState yourName={"Heaven"} /> */}
 
-        {/* <h2>Animals</h2>
-        <article className="animals">
-            <AnimalCard />
-            <AnimalCard />
-            <AnimalCard />
-        </article>
+//         {/* <h2>Animals</h2>
+//         <article className="animals">
+//             <AnimalCard />
+//             <AnimalCard />
+//             <AnimalCard />
+//         </article>
 
-        <h2>Employees</h2>
-        <article className="employees">
-            <EmployeeCard />
-            <EmployeeCard />
-            <EmployeeCard />
-            </article>
+//         <h2>Employees</h2>
+//         <article className="employees">
+//             <EmployeeCard />
+//             <EmployeeCard />
+//             <EmployeeCard />
+//             </article>
 
-        <h2>Locations</h2>
-        <article className="locations">
-            <LocationCard />
-            <LocationCard />
+//         <h2>Locations</h2>
+//         <article className="locations">
+//             <LocationCard />
+//             <LocationCard />
             
 
-        </article>
+//         </article>
 
         
 
-        <h2>Customers</h2>
-        <article className="customers">
-            <CustomerCard />
-            <CustomerCard />
-            <CustomerCard />
-            <CustomerCard />
+//         <h2>Customers</h2>
+//         <article className="customers">
+//             <CustomerCard />
+//             <CustomerCard />
+//             <CustomerCard />
+//             <CustomerCard />
             
 
-        </article> */}
+//         </article> */}
 
-    </>
-)
+//     </>
+// )
 
 
 
